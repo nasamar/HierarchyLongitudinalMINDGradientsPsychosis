eebad960476bffbc5fc3a89b1bd00055cabbaaec
Natalia García-San-Martín <132894441+nasamar@users.noreply.github.com>
2026-01-29 14:34:59 +0100

    Update MIND_long_01_MIND.py

--- Code/MIND_long_01_MIND.py
+++ Code/MIND_long_01_MIND.py
@@ -11,20 +11,20 @@ Copyright (C) 2026 University of Seville
 
 Written by Natalia García San Martín (ngarcia1@us.es)
 
-This file is part of Hierarchy Longitudinal Gradients Psychosis toolkit.
+This file is part of Hierarchy Longitudinal MIND Gradients Psychosis toolkit.
 
-Hierarchy Longitudinal Gradients Psychosis toolkit is free software: 
+Hierarchy Longitudinal MIND Gradients Psychosis toolkit is free software: 
 you can redistribute it and/or modify it under the terms of the 
 GNU General Public License as published by the Free Software Foundation, 
 either version 3 of the License, or (at your option) any later version.
 
-Hierarchy Longitudinal Gradients Psychosis toolkit is distributed in the hope that 
+Hierarchy Longitudinal MIND Gradients Psychosis toolkit is distributed in the hope that 
 it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Hierarchy Longitudinal Gradients Psychosis toolkit. If not, see 
+along with Hierarchy Longitudinal MIND Gradients Psychosis toolkit. If not, see 
 <https://www.gnu.org/licenses/>.
 
 """
@@ -67,3 +67,4 @@ for subject in folders:
         errors.append(subject)
         continue
     
+
